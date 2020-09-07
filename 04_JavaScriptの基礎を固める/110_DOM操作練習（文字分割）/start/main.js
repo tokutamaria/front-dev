ade75014cf20b0e48af126083d2940439a2ee293
Takuto Arima <ta910arima@gmail.com>
2020-09-07 11:04:18 +0900

    js

---
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -0,0 +1,19 @@
+'use strict'
+{
+  document.addEventListener('DOMContentLoaded', function() {
+    const el = document.querySelector('.animate-title');
+    const str = el.innerHTML.trim().split("");
+
+    // let concatStr = '';
+
+    // for(let c of str) {
+    //   c = c.replace('', '&nbsp;');
+    //   concatStr += `<span class="char">${c}</span>`;
+    // }
+    el.innerHTML = str.reduce((acc, curr) => {
+      curr = curr.replace(/\s+/, '&nbsp;');
+      return `${acc}<span class="char">${c}</span>`;
+    }, "");
+  });
+
+}
