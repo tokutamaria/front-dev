57ace12b23fc480582549f27a667c9a721f84b4a
Takuto Arima <ta910arima@gmail.com>
2020-09-09 16:50:21 +0900

    変更

---
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -0,0 +1,10 @@
+let mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
